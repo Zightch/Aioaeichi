1e1a739ec98d2d291748b893ba3d5e759ddb1bb2
Zightch <Zightch@163.com>
2023-08-01 20:31:22 +0800

    delete GUI

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
-#include<QGuiApplication>
+#include<QCoreApplication>
 #include"Aioaeichi/ConsoleCommand/ConsoleCommand.h"
 #include"Aioaeichi/CommandProcessing/CommandProcessing.h"
 #include"Aioaeichi/PluginsManager/PluginsManager.h"
 #include<iostream>
 int main(int argc, char *argv[]) {
-    QGuiApplication a(argc, argv);
+    QCoreApplication a(argc, argv);
     CommandProcessing cp;
     PluginsManager pm(&cp);
     auto cc = ConsoleCommand::getObjetc(&cp);
-    QObject::connect(&cp, &CommandProcessing::exit, &a, &QGuiApplication::quit);
+    QObject::connect(&cp, &CommandProcessing::exit, &a, &QCoreApplication::quit);
     cc->start();
-    int exec = QGuiApplication::exec();
+    int exec = QCoreApplication::exec();
     ConsoleCommand::deleteThis();
     std::cout << std::endl;
     return exec;
