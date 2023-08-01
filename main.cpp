d79ee8acabb9f99b1fe224f05f2eb23c70f960ec
Zightch <zightch@163.com>
2023-08-01 17:48:33 +0800

    加入Gui依赖库

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
-#include<QCoreApplication>
+#include<QGuiApplication>
 #include"Aioaeichi/ConsoleCommand/ConsoleCommand.h"
 #include"Aioaeichi/CommandProcessing/CommandProcessing.h"
 #include"Aioaeichi/PluginsManager/PluginsManager.h"
 #include<iostream>
 int main(int argc, char *argv[]) {
-    QCoreApplication a(argc, argv);
+    QGuiApplication a(argc, argv);
     CommandProcessing cp;
     PluginsManager pm(&cp);
     auto cc = ConsoleCommand::getObjetc(&cp);
-    QObject::connect(&cp, &CommandProcessing::exit, &a, &QCoreApplication::quit);
+    QObject::connect(&cp, &CommandProcessing::exit, &a, &QGuiApplication::quit);
     cc->start();
-    int exec = QCoreApplication::exec();
+    int exec = QGuiApplication::exec();
     ConsoleCommand::deleteThis();
     std::cout << std::endl;
     return exec;
